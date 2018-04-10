8654530a406e7c173ce9c485ce0fb727acc99d48
root <you@example.com>
2018-04-10 13:23:43 +0800

    1

---
+++ demo16/server/videoprocessor.h
@@ -0,0 +1,34 @@
+#ifndef VIDEOPROCESSOR_H
+#define VIDEOPROCESSOR_H
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include "tool.h"
+using namespace std;
+using namespace cv;
+class VideoProcessor
+{
+
+public:
+    VideoProcessor()
+    {
+
+    }
+    virtual  bool real_process( Mat &, std::vector<cv::Rect> &)
+    {
+
+    }
+
+    virtual void init()
+    {
+
+    }
+protected:
+};
+
+
+#endif // VIDEOPROCESSOR_H
